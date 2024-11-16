2addc6c53d0a2de939918aaf68ce382b7d8bd114
Desmond Isama <desmond.isama@decagon.dev>
2024-11-16 23:42:57 +0100

    reset password

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { register } from '../controllers/authController';
+import { register, resetPassword } from '../controllers/authController';
 
 const router = Router();
 
 router.post('/register', register);
+router.post('/reset-password/:token', resetPassword);
+
 
 
 export default router;
