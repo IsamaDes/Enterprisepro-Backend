e1851403651e47fe69966c855d2f8eac442fec92
Desmond Isama <desmond.isama@decagon.dev>
2024-11-16 22:31:32 +0100

    node reinstalled

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,6 +4,7 @@ import { register, login, resetPassword } from '../controllers/authController';
 const router = Router();
 
 router.post('/register', register);
-router.post('/login', login)
+router.post('/login', login);
+router.post('/resetPassword', resetPassword) 
 
 export default router;
