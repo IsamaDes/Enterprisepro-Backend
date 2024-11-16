ce890d73983a3848cdead6ce6b03c173b31b0b27
Desmond Isama <desmond.isama@decagon.dev>
2024-11-17 00:46:45 +0100

    login added

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 import User from '../models/userModel';
 // import errorHandler from '../middleware/erroHandler'; 
 
@@ -36,20 +36,14 @@ interface DecodedToken {
 };
 
 // Login
-export const login = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => { 
-    const { email, password } = req.body; 
-    try { 
-        const user = await User.findOne({ email }).exec(); 
-        if (!user) { 
-            return res.status(400).json({ message: 'Invalid credentials' }); 
-        } const isMatch = await bcrypt.compare(password, user.password); 
-        if (!isMatch) { return res.status(400).json({ message: 'Invalid credentials' }); 
-    } const token = jwt.sign( { userId: user._id, role: user.role }, 
-        process.env.JWT_SECRET as string, { expiresIn: '1h' } ); 
-        return res.status(200).json({ message: 'Login successful', token }); 
-    } catch (error) { console.error('Error during login:', error); 
-        return res.status(500).json({ message: 'Error logging in', error }); } };
-
+export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      // your login logic here
+      res.status(200).json({ message: 'User logged in successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
 
 
 
