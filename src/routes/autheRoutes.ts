9943f706951aa6a8ad4ef8cfdd5455c647159313
Desmond Isama <desmond.isama@decagon.dev>
2024-11-17 02:34:00 +0100

    login

--- src/routes/autheRoutes.ts
+++ src/routes/autheRoutes.ts
@@ -1,10 +1,35 @@
-import { Router } from 'express';
-import { register, login,  } from '../controllers/autheController';
 
-const router = Router();
+import express, { Request, Response } from 'express';
+import {  IUser } from '../models/userModel'; // Assuming you have a User model
+import { register  } from '../controllers/autheController';
+import User from '../models/userModel';
 
+
+
+const router = express.Router();
 router.post('/register', register);
-router.post('/login', login);
-// router.post('/reset-password/:token', resetPassword);
+
+
+router.post('/login', async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (user.password !== password) {
+            return res.status(401).json({ message: 'Incorrect password' });
+        }
+
+        // Successful login, you can generate a token or session here
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
 export default router;
