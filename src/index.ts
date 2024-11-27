be5648f4cdaf305b6a841efa097c375628b61b23
Desmond Isama <desmond.isama@decagon.dev>
2024-11-27 11:08:43 +0100

    route for confirmation

--- src/index.ts
+++ src/index.ts
@@ -63,30 +63,7 @@ app.options('*', cors(corsOptions)); // Allow pre-flight requests for all routes
 app.use(express.json());
 app.use('/api/business', businessRoutes);
 app.use('/api/auth', authRoutes);
-app.get('/api/confirmation/:token', async (req: Request, res: Response) => { 
-  try { 
-      console.log('Confirmation route hit');
-  const { token } = req.params; 
-  const secret = process.env.JWT_SECRET
 
-  if (!secret) { 
-      throw new Error('JWT_SECRET is not defined'); 
-  }
-  const decoded = jwt.verify(token, secret) as JwtPayload; 
-  const userId = decoded.id;
-
-  if (!userId) { 
-      console.error('Invalid token');
-      return res.status(400).send('Invalid token'); }
-
-  await User.findByIdAndUpdate(userId, { isVerified: true }); 
-  console.log('User verified:', userId);
-  return res.redirect('https://enterprisepro-backend.onrender.com/login'); 
-} catch (err) { 
-  console.error('Error during confirmation:', err);
-  return res.status(400).send('Invalid token'); 
-}
-});
 
 app.use('/api', loginRoute);
 
