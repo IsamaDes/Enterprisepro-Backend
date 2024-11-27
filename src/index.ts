04ce38c8fffd34d83983d27cc2a6f933448cd2dd
Desmond Isama <desmond.isama@decagon.dev>
2024-11-27 04:38:12 +0100

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ import mongoSanitize from 'express-mongo-sanitize'; // Assuming you're using exp
 import passport from 'passport'; // If using Passport.js for authentication
 import sanitizeInputs from './middleware/sanitizeInputs'; // Custom input sanitization middleware
 import logRequest from './middleware/logRequest'; // Custom request logging middleware
-import confirmationRoute from './routes/confirmationRoute';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import User from './entity/User';
 
 
 
@@ -62,7 +63,30 @@ app.options('*', cors(corsOptions)); // Allow pre-flight requests for all routes
 app.use(express.json());
 app.use('/api/business', businessRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/confirmation', confirmationRoute);
+app.get('/api/confirmation/:token', async (req: Request, res: Response) => { 
+  try { 
+      console.log('Confirmation route hit');
+  const { token } = req.params; 
+  const secret = process.env.JWT_SECRET
+
+  if (!secret) { 
+      throw new Error('JWT_SECRET is not defined'); 
+  }
+  const decoded = jwt.verify(token, secret) as JwtPayload; 
+  const userId = decoded.id;
+
+  if (!userId) { 
+      console.error('Invalid token');
+      return res.status(400).send('Invalid token'); }
+
+  await User.findByIdAndUpdate(userId, { isVerified: true }); 
+  console.log('User verified:', userId);
+  return res.redirect('/login'); 
+} catch (err) { 
+  console.error('Error during confirmation:', err);
+  return res.status(400).send('Invalid token'); 
+}
+});
 
 app.use('/api', loginRoute);
 
