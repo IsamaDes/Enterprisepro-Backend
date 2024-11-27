a716c44126df1bbbd3c42b49a04e2c3467bf256b
Desmond Isama <desmond.isama@decagon.dev>
2024-11-27 02:30:23 +0100

    corrected jwtpayload error

--- src/routes/confirmation.ts
+++ src/routes/confirmation.ts
@@ -1,11 +1,11 @@
 // routes/confirmation.js
-import express from 'express';
-import jwt from 'jsonwebtoken';
+import express, { Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import User from '../entity/User';
 
 const router = express.Router();
 
-router.get('/confirmation/:token', async (req, res) => { 
+router.get('/confirmation/:token', async (req: Request, res: Response) => { 
     try { 
     const { token } = req.params; 
     const secret = process.env.JWT_SECRET
@@ -13,8 +13,11 @@ router.get('/confirmation/:token', async (req, res) => {
     if (!secret) { 
         throw new Error('JWT_SECRET is not defined'); 
     }
-    const decoded = jwt.verify(token, secret) as jwt.JwtPayload; 
+    const decoded = jwt.verify(token, secret) as JwtPayload; 
     const userId = decoded.id;
+
+    if (!userId) { return res.status(400).send('Invalid token'); }
+    
     await User.findByIdAndUpdate(userId, { isVerified: true }); 
     return res.redirect('/login'); 
 } catch (err) { 
